7fc7eeadbd8580e7b20ca3e9770d6cba69f0245b
Ian Brandeberry <brandeberry@battelle.org>
2017-10-31 16:06:48 -0400

    Updated tenda scraper

--- firmware/spiders/tenda_en.py
+++ firmware/spiders/tenda_en.py
@@ -13,46 +13,45 @@ class TendaENSpider(Spider):
     vendor = "tenda"
     allowed_domains = ["tendacn.com"]
     start_urls = ["http://www.tendacn.com/en/service/download-cata-11-1.html"]
-    url = "http://www.tendacn.com/en/service/download-cata-11-{}.html"
+    url = "http://www.tendacn.com/en/service/download-cata-11-1.html"
 
     def parse(self, response):
-        for page in response.xpath("//div[@class='next-page']/a/text()").extract():
+        for page in response.xpath("//*[contains(@class, 'dr_file')]//@href").extract():
             yield Request(
-                url=self.url.format(page),
+                url=page,
                 headers={"Referer": response.url,
                          "X-Requested-With": "XMLHttpRequest"},
                 callback=self.parse_product)
 
     def parse_product(self, response):
-        for a in response.xpath("//div[@id='mainbox']//dd/a"):
-            url = a.xpath("./@href").extract()[0]
-            title = a.xpath("./text()").extract()[0]
-            description = title
+        url = response.xpath("//*[contains(@class, 'downbtns')]//@href").extract()[0]
+        title = response.xpath("//*[contains(@class, 'downdes')]/h1/text()").extract()[0]
+        description = title
 
-            items = title.split(' ')
-            product = items[0]
-            version = items[-1]
+        items = title.split(' ')
+        product = items[0]
+        version = items[-1]
 
-            #FH456V1.0 Firmware V10.1.1.1_EN
-            #E101（V2.0） Firmware V1.10.0.1_EN
-            #G3(V2.0） Firmware V2.0.0.1_EN
-            #O3 Firmware V1.0.0.3_EN
-            #i6 Firmware V1.0.0.9(3857)_EN
-            import re
-            p = ur'^(?P<product>([a-uw-zA-UW-Z0-9])+)[\(\uff08]?(V\d\.0)?'
-            try:
-                ret = re.search(p, items[0].decode('utf-8'))
+        #FH456V1.0 Firmware V10.1.1.1_EN
+        #E101（V2.0） Firmware V1.10.0.1_EN
+        #G3(V2.0） Firmware V2.0.0.1_EN
+        #O3 Firmware V1.0.0.3_EN
+        #i6 Firmware V1.0.0.9(3857)_EN
+        import re
+        p = ur'^(?P<product>([a-uw-zA-UW-Z0-9])+)[\(\uff08]?(V\d\.0)?'
+        try:
+            ret = re.search(p, items[0].decode('utf-8'))
 
-                if ret:
-                    product = ret.group('product')
-            except:
-                product = item[0]
+            if ret:
+                product = ret.group('product')
+        except:
+            product = item[0]
 
-            item = FirmwareLoader(
-                item=FirmwareImage(), response=response)
-            item.add_value(
-                "version", version)
-            item.add_value("url", url)
-            item.add_value("product", product)
-            item.add_value("vendor", self.vendor)
-            yield item.load_item()
+        item = FirmwareLoader(
+            item=FirmwareImage(), response=response)
+        item.add_value(
+            "version", version)
+        item.add_value("url", url)
+        item.add_value("product", product)
+        item.add_value("vendor", self.vendor)
+        yield item.load_item()
